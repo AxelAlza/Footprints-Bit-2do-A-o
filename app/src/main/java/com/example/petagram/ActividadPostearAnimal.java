2244687af49e5860f51d4abeedfa794904b16f83
AxelAlza <axelalza1998@gmail.com>
2021-03-26 03:31:21 -0300

    Agregada Actividad para postear animal con su layout terminado

---
+++ app/src/main/java/com/example/petagram/ActividadPostearAnimal.java
@@ -0,0 +1,25 @@
+package com.example.petagram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class ActividadPostearAnimal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_postear_animal);
+    }
+
+    ///Este metodo se dispara cada vez que se aprieta un radioboton
+    public void RadioBotonTamañoClickeado(View view) {
+
+    }
+
+    public void RadioBotonGeneroClickeado(View view) {
+
+    }
+
+}
